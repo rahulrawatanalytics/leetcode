a001d27f2a4e1fb2a694406f8f97c4d7b0e62aa4
Rahul Rawat <50006637+rahulrawatanalytics@users.noreply.github.com>
2024-07-11 14:12:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,3 @@
 /* Write your T-SQL query statement below */
- with manager_reports as ( select managerId , Count(*) as count_direct_reports from Employee where managerId is not null group by managerID having count(*)>4 ) 
- 
- select name from Employee where id in (select managerId from manager_reports)
+ with manager_reports as ( select managerId , Count(*) as count_direct_reports from Employee where managerId is not null group by managerID ) 
+ select name from Employee where managerId in (select managerId from manager_reports where count_direct_reports >4 )
