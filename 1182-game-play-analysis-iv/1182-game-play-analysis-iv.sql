1986b443a97e76a2ea5f28b0928d319b10b4d2c3
Rahul Rawat <50006637+rahulrawatanalytics@users.noreply.github.com>
2024-07-15 12:58:58 +0530

    Time: 548 ms (72.19%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
 select 
-round( (count(distinct player_id)/ ( select count(distinct player_id) from Activity ) ) , 2)   as fraction
+round( count(distinct player_id)/ ( select count(distinct player_id) from Activity )  , 2)   as fraction
 from 
 Activity
 where (player_id, date_sub(event_date, interval 1 day))
